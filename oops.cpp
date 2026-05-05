9077ede02b787fac2c6f51c7501ef6a974201c17
Divanshi Goyal <divanshigoyal051209@gmail.com>
2026-05-05 21:03:33 +0530

    dsa

--- oops.cpp
+++ oops.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 class student{
-    public:
+    private:
     int id;
     string name;
     int age;
+
     
+    string gf;
+
+    public:
     // parameterized constructor
-    student(int id, string name, int age){
+    student(int id, string name, int age, string gf){
         this->id=id;
         this->name= name;
         this->age=age;
@@ -32,18 +36,28 @@ class student{
     void study(){
         cout<< this->name <<" is studying"<<endl;
     }
+    void setage(int age){
+        this->age=age;
+    }
+    void getage() const{
+        cout<< this->name<<" is "<<this->age<<" years old"<<endl;
+    }
+    private:
+    void secret(){
+        cout<<"This is a secret function"<<endl;
+    }
 };
 
 int main(){
     // static memory allocation
-    student s1(513, "Divanshi Goyal",20);
-    student s2(s1);
-    cout<< s2.name<<endl;
-    s1.study();
+    student s1(513, "Divanshi Goyal",20,"meenu");
+    student s2(s1); // or student s2= s1; // copy constructor is called
+    s1.getage();
+    //cout<< s2.gf<<endl; // error: 'gf' is a private member of 'student'
 
     // dynamic memory allocation
-    student *s3= new student(510, "Dilnaz", 21);
-    cout<<s3->name<<endl;
+    student *s3= new student(510, "Dilnaz", 21, "priya");
+    s3->getage();
     delete s3;
     
     return 0;
